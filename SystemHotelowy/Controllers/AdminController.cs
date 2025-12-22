e5053f80fab2734b8f87806833c5594dd40b79d0
Eliza Lenik <lenikeliza@gmail.com>
2025-12-23 00:13:54 +0100

    Dodaj pliki projektów.

---
+++ SystemHotelowy/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SystemHotelowy.Areas.Identity.Data;
+
+namespace SystemHotelowy.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GiveRole(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Receptionist");
+            }
+            return RedirectToAction("Index"); // Przekieruje do listy użytkowników
+        }
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
