0018b2214bb398752118bd2960efc9220dbdf432
Eliza Lenik <lenikeliza@gmail.com>
2026-01-11 14:59:26 +0100

    Last changes

--- SystemHotelowy/Areas/Identity/Data/ApplicationDBContext.cs
+++ SystemHotelowy/Areas/Identity/Data/ApplicationDBContext.cs
@@ -27,9 +27,6 @@ public class ApplicationDBContext : IdentityDbContext<AppUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
         builder.Entity<IdentityRole>().HasData(
             new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
             new IdentityRole { Id = "2", Name = "Receptionist", NormalizedName = "RECEPTIONIST" },
